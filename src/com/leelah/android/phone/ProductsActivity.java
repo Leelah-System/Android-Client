9539f953697f979c468d732b517795a32c0bee4f
Eri <jocelyn@erioxyde.com>
2012-08-17 23:57:35 +0200

    Now supporting multiple screen/handset, with phone & tablet display mode.

---
+++ src/com/leelah/android/phone/ProductsActivity.java
@@ -0,0 +1,54 @@
+package com.leelah.android.phone;
+
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+
+import com.leelah.android.Bar.BarRefreshFeature;
+import com.leelah.android.Bar.BarShowHomeFeature;
+import com.leelah.android.LeelahFragmentActivity;
+import com.leelah.android.R;
+import com.leelah.android.fragments.ProductsListFragment;
+
+public final class ProductsActivity
+    extends LeelahFragmentActivity
+    implements BarRefreshFeature, BarShowHomeFeature
+{
+
+  private ProductsListFragment productsFragment;
+
+  @Override
+  public void onRetrieveDisplayObjects()
+  {
+    final FragmentManager fragmentManager = getSupportFragmentManager();
+    final FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+    fragmentTransaction.add(android.R.id.content, new ProductsListFragment());
+    fragmentTransaction.commit();
+  }
+
+  @Override
+  public void onRetrieveBusinessObjects()
+      throws BusinessObjectUnavailableException
+  {
+    super.onRetrieveBusinessObjects();
+
+    productsFragment = (ProductsListFragment) getSupportFragmentManager().findFragmentById(R.id.productsFragment);
+  }
+
+  @Override
+  public void onFulfillDisplayObjects()
+  {
+
+  }
+
+  @Override
+  public void onSynchronizeDisplayObjects()
+  {
+
+  }
+
+  public void onTitleBarRefresh()
+  {
+    productsFragment.onTitleBarRefresh();
+  }
+
+}
